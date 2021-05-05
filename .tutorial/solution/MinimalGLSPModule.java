d5af46161b999dc2df57665fadeb1f42a20b2e74
Max Elia <max_elia@hotmail.de>
2021-05-05 06:50:45 +0200

    Contribute glsp-tutorial
    * Add all the files and gitpod configuration for loading the tutorial files together with the theia-tutorial-extension
    * Change Dockerfile to choose lower node version since newer node versions are not compatible with the glsp-tutorial
    * Closes eclipsesource/theia-tutorial-extension#14
    * Switch to glsp-example repo without assistance
    * Host images on github
    * Update Year
    * Change test folder structure
    * Change prebuild task
    * Update Readme
    * Update .tut.json to adjust to changes
    Closes #2
    *Add prettier and eslint rules
    *Update .tutorial/glsp.tut.json
    
    Signed-off-by: Max Elia Schweigkofler <max_elia@hotmail.de>
    Co-authored-by: Eugen Neufeld <neufeld.eugen@googlemail.com>

---
+++ .tutorial/solution/MinimalGLSPModule.java
@@ -0,0 +1,46 @@
+/********************************************************************************
+ * Copyright (c) 2021 EclipseSource and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+package org.eclipse.glsp.example.minimal;
+
+import org.eclipse.glsp.example.minimal.handler.MinimalCreateNewNodeOperationHandler;
+import org.eclipse.glsp.example.minimal.handler.MinimalCreateNodeOperationHandler;
+import org.eclipse.glsp.server.di.DefaultGLSPModule;
+import org.eclipse.glsp.server.diagram.DiagramConfiguration;
+import org.eclipse.glsp.server.features.core.model.JsonFileModelFactory;
+import org.eclipse.glsp.server.features.core.model.ModelFactory;
+import org.eclipse.glsp.server.operations.OperationHandler;
+import org.eclipse.glsp.server.utils.MultiBinding;
+
+public class MinimalGLSPModule extends DefaultGLSPModule {
+
+   @Override
+   protected void configureDiagramConfigurations(final MultiBinding<DiagramConfiguration> binding) {
+      binding.add(MinimalDiagramConfiguration.class);
+   }
+
+   @Override
+   protected void configureOperationHandlers(final MultiBinding<OperationHandler> binding) {
+      super.configureOperationHandlers(binding);
+      binding.add(MinimalCreateNodeOperationHandler.class);
+      binding.add(MinimalCreateNewNodeOperationHandler.class);
+   }
+
+   @Override
+   protected Class<? extends ModelFactory> bindModelFactory() {
+      return JsonFileModelFactory.class;
+   }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# glsp-tutorial
+# GLSP Tutorial
+
+## Eclipse Graphical Language Server Platform (GLSP)
+
+The [Graphical Language Server Platform (GLSP)](https://github.com/eclipse-glsp/glsp) provides extensible components for the development of diagram editors including edit functionality in (distributed) web-applications via a client-server protocol.
+This repository contains the necessary files for following the tutorial based on the [GLSP Examples](https://github.com/eclipse-glsp/glsp-examples) repository. The tutorial will be displayed inside the [Tutorial Extension](https://github.com/eclipsesource/theia-tutorial-extension).
+
+### [Start tutorial](https://gitpod.io/#https://github.com/eclipsesource/glsp-tutorial)
+
+![alt text](https://www.eclipse.org/glsp/images/diagramanimated.gif)
+
+## License
+
+- [Eclipse Public License 2.0](LICENSE)
+- [The MIT License](https://opensource.org/licenses/MIT)
+
+[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/eclipsesource/glsp-tutorial)
